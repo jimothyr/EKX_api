e1d25104f89c5dba7aebb1803588d2bef3d6a656
James Perry <jamesperry@Jamess-MBP.lan>
2017-05-17 22:35:05 +0100

    summary

--- services/keywords/get_keywords.js
+++ services/keywords/get_keywords.js
@@ -1,34 +1,15 @@
-// var retext = require('retext');
-// var keywords = require('retext-keywords');
-// var nlcstToString = require('nlcst-to-string');
+var keyword_extractor = require("keyword-extractor");
 
 exports.get_keywords = function(text){
-	// return new Promise(function (resolve, reject) {
-	// 	retext().use(keywords, {maximum:100}).process(text, function(err, file){
-	// 		if(err){
-	// 			return reject(err);
-	// 		}
-
-	// 		var retKeys = [];
-	// 		file.data.keyphrases.forEach(function (phrase) {
-	// 			phrase.sort = phrase.score*phrase.weight;
-	// 			if(phrase.score > 0.015){
-	// 		    	// retKeys.push({term: phrase.matches[0].nodes.map(nlcstToString).join('').toLowerCase(), score: phrase.score, weight: phrase.weight, sort: phrase.sort});
-	// 		    	retKeys.push({term: phrase.matches[0].nodes, score: phrase.score, weight: phrase.weight, sort: phrase.sort});
-	// 			}
-	// 	    });
-		    
-	// 		retKeys.sort(function compare(a,b) {
-	// 		  if (a.sort > b.sort)
-	// 		    return -1;
-	// 		  if (a.sort < b.sort)
-	// 		    return 1;
-	// 		  return 0;
-	// 		});
-	// 	    return resolve(retKeys);
-	// 	})
-	// }).catch((error) => {
-	//     console.log('get Keywords - ', error)
-	// 	return reject(error);
-	// })
+	return new Promise(function (resolve, reject) {
+		return resolve(keyword_extractor.extract(text,{
+            language:"english",
+            remove_digits: true,
+            return_changed_case:true,
+            remove_duplicates: true
+       }));
+	}).catch((error) => {
+	    console.log('get Keywords - ', error)
+		return reject(error);
+	})
 }
