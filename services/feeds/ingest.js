d2b65592ceb933295e7f1de591a120816bcf0c00
rsrc1147 <jperry1147@gmail.com>
2017-06-15 11:47:24 +0100

    events

--- services/feeds/ingest.js
+++ services/feeds/ingest.js
@@ -4,7 +4,7 @@ var FeedParser    = require('feedparser'),
 var search_engine = require('../search/elasticsearch'),
     content       = require('../content/get_content'), 
     services      = require('../services/get_services');
-    events        = require('../services/events/eventbrite');
+    events        = require('../events/eventbrite');
 
 
 // 
