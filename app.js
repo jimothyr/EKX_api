0cc29dd4dacdefedb5c53dcaf182fb75f70484e1
rsrc1147 <jperry1147@gmail.com>
2017-07-31 12:46:08 +0100

    added provider and feed data to ingest and added rss check route

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ var app = express();
       });
 
       // --------------------------------------------------------┤ CHECK IF AN RSS FEED IS A VALID RSS FEED
-      app.get('/rss/check/:feed', function(req,res){
+      app.post('/rss/check/:feed', function(req,res){
         res.setHeader('Access-Control-Allow-Origin','*');
         res.setHeader('Content-Type', 'application/json');
         res.send(ingest.check_valid_feed(req.params.feed));
