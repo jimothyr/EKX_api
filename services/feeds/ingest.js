fb02b9d0e0f9a8a70b5af7d68b4c6fdc9d664705
rsrc1147 <jperry1147@gmail.com>
2017-07-31 13:40:46 +0100

    added provider and feed data to ingest and added rss check route

--- services/feeds/ingest.js
+++ services/feeds/ingest.js
@@ -290,12 +290,18 @@ exports.ingest_feeds = function(provider_id){
 }
 
 exports.check_valid_feed = function(feed_url){
-  request({
-      url: feed_url,
-      method: "GET",
-    }, 
-    function (error, response, body){
-      return(response) // 'image/png' 
-    });
-  
+  // var req = request(feed_url);
+  // var feedparser = new FeedParser();
+  // req.on('error', function (error) {
+  //   reject(error);
+  // });
+
+  // req.on('response', function (res) {
+  //   return res;
+  // });
+  request.get(feed_url)
+  .on('response', function(response) {
+    // console.log(response.statusCode) // 200 
+    return(response.headers['content-type']) // 'image/png' 
+  })
 }
