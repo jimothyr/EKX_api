af41b682104ded66b491488827800c8b2c0ca944
rsrc1147 <jperry1147@gmail.com>
2017-08-03 15:24:00 +0100

    ukerc demo

--- services/ukerc/ukerc.js
+++ services/ukerc/ukerc.js
@@ -3,19 +3,22 @@ var request = require('request');
 
 exports.getSearchPost = function(keywords){
 	return new Promise(function (resolve, reject) {
-		request({
-		    url: 'http://ukerc.rl.ac.uk/cgi-bin/era001.pl',
-			method: "POST",
-			body: {'STerm' : keywords},
-		    // json: true,   // <--Very important!!
-		}, function (error, response, body){
-			if(error){
-				return reject(error);
-			}
-			return resolve(body);
-		});
-	}).catch((error) => {
-        console.log('organisations - ', error)
-		return reject(error);
-    })
+		// request({
+		//     url: 'http://ukerc.rl.ac.uk/cgi-bin/era001.pl',
+		// 	method: "POST",
+		// 	body: 'STerm='+keywords,
+		//     // json: true,   // <--Very important!!
+		// }, function (error, response, body){
+		// 	if(error){
+		// 		return reject(error);
+		// 	}
+		// 	return resolve(body);
+        // });
+        request.post({url:'http://ukerc.rl.ac.uk/cgi-bin/era001.pl', formData: {STerm:keywords}}, function optionalCallback(err, httpResponse, body) {
+        if (err) {
+            return console.error('upload failed:', err);
+        }
+        return resolve(body);
+        });
+	})
 }
