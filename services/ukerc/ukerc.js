44718dd914d58f8f7524a5321d226355eeeb7a2a
rsrc1147 <jperry1147@gmail.com>
2017-08-03 16:16:45 +0100

    ukerc demo

--- services/ukerc/ukerc.js
+++ services/ukerc/ukerc.js
@@ -1,7 +1,5 @@
 var request = require('request');
 var cheerio = require('cheerio');
-// var unfluff = require('unfluff');
-// var search = require('../search/elasticsearch');
 
 exports.getSearchPost = function(keywords){
 	return new Promise(function (resolve, reject) {
@@ -13,17 +11,9 @@ exports.getSearchPost = function(keywords){
         var retArr = [];
         $('.resultslist').each(function(i, elem){
             $(this).find('a').each(function(i, elem){
-                console.log($(this));
                 retArr.push({url: $(this).attr('href'), text: $(this).text()});
             })
         })
-        // var HTMLBody = unfluff(body);
-        // var links = HTMLBody.links || [];
-        // links.forEach(function(l,i){
-        //     if(l.href.includes('://') && l.text != 'Remove all Filters'){
-        //         retArr.push(l);
-        //     }
-        // })
         return resolve({links: retArr, raw: body});
         });
 	})
