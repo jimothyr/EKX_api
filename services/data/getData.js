df0fc695150a46c5ad62fbfe52713e60d71f7733
rsrc1147 <jperry1147@gmail.com>
2017-08-09 10:17:13 +0100

    datasets

--- services/data/getData.js
+++ services/data/getData.js
@@ -2,11 +2,13 @@ var search      = require('../search/elasticsearch'),
     appGlobals 	= require('../globals/globals.json');
 
 exports.get_data = function(keywords){
-    search.getRelated(keywords, appGlobals.dataShard)
-    .then(function(ret_related){
-        return resolve(ret_related)
-    }).catch((error) => {
-        console.log('related - ', error)
-        return reject(error);
-    });
+    return new Promise(function (resolve, reject) {
+        search.getRelated(keywords, appGlobals.dataShard)
+        .then(function(ret_related){
+            return resolve(ret_related)
+        }).catch((error) => {
+            console.log('related - ', error)
+            return reject(error);
+        });
+    })
 }
