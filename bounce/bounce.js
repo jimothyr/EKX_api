f5f31e5afb04df1d3b2201c71ed171038f1cd2f3
rsrc1147 <jperry1147@gmail.com>
2017-08-15 18:10:39 +0100

    getpost test

--- bounce/bounce.js
+++ bounce/bounce.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 exports.bounce = function(strings){
-	fs.writeFile(process.env.OPENSHIFT_DATA_DIR+'Bounce/bounces.csv', JSON.stringify(strings)+'\n',  {'flag':'a'},  function(err) {
+	fs.writeFile(process.env.OPENSHIFT_DATA_DIR+'Bounce/bounces.txt', JSON.stringify(strings)+'\n',  {'flag':'a'},  function(err) {
 		if(err){
 			console.log('bounce error :' ,err)
 		}
